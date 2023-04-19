d5b2a9479db86fe115ba6083bd35b3906ba31294
Stefan Ponko <ponko3@comcast.net>
2023-04-19 14:24:16 -0700

    updated to keep login logout on right of navbar

--- services/frontend/src/components/NavBar.vue
+++ services/frontend/src/components/NavBar.vue
@@ -7,27 +7,26 @@
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarCollapse">
-          <ul v-if="isLoggedIn" class="navbar-nav me-auto mb-2 mb-md-0">
-            <li class="nav-item">
-              <router-link class="nav-link" to="/">Home</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/dashboard">Dashboard</router-link>
-            </li>
-            <li class="nav-item">
-              <router-link class="nav-link" to="/profile">My Profile</router-link>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" @click="logout">Log Out</a>
-            </li>
-          </ul>
-          <ul v-else class="navbar-nav me-auto mb-2 mb-md-0">
+          <ul class="navbar-nav me-auto mb-2 mb-md-0">
             <li class="nav-item">
               <router-link class="nav-link" to="/">Home</router-link>
             </li>
             <li class="nav-item">
               <router-link class="nav-link" to="/register">Register</router-link>
             </li>
+            <li v-if="isLoggedIn" class="nav-item">
+              <router-link class="nav-link" to="/dashboard">Dashboard</router-link>
+            </li>
+            <li v-if="isLoggedIn" class="nav-item">
+              <router-link class="nav-link" to="/profile">My Profile</router-link>
+            </li>
+          </ul>
+          <ul v-if="isLoggedIn" class="navbar-nav mr-auto mb-2 mb-md-0">
+            <li class="nav-item">
+              <a class="nav-link" @click="logout">Log Out</a>
+            </li>
+          </ul>
+          <ul v-else class="navbar-nav mr-auto mb-2 mb-md-0">
             <li class="nav-item">
               <router-link class="nav-link" to="/login">Log In</router-link>
             </li>
